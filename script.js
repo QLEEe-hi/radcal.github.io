b05f36eeeabb0ac9d447238f6e4ab883eb0211f2
QLEEe-hi <96573134+QLEEe-hi@users.noreply.github.com>
2023-12-16 19:24:36 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -13,15 +13,22 @@ function BED(){
     var ab = parseFloat(abEle.value).toFixed(2);
 
     
-    if (PhanLieu<0 || TongLieu<0 || ab < 0 ||isNaN(PhanLieu)||isNaN(TongLieu)||isNaN(ab)){
-        alert('Hãy kiểm tra lại dữ liệu đã nhập vào');
-        EQD2 = 0;
-        BED = 0;}
-
+    if (PhanLieu<0 || TongLieu<0 || ab < 0){
+        if (ab<=0){
+            alert('Hãy kiểm tra lại dữ liệu đã nhập vào');
+            EQD2 = 0;
+            BED = 0;
+        };
+    }        
     var BED = Math.round((TongLieu * (1 + PhanLieu/ab))*100)/100;
 
     var EQD2 = Math.round(((TongLieu*(1+(PhanLieu/ab)))/(1+(2/ab)))*100)/100;
 
+    if (isNaN(BED)||isNaN(EQD2)){
+        alert('Hãy kiểm tra lại dữ liệu đã nhập vào');
+            EQD2 = 0;
+            BED = 0;
+    };
 
     document.getElementById("kqBED").value = BED.toString();
     document.getElementById("kqeqd2").value = EQD2.toString();
@@ -54,20 +61,20 @@ function ChuyenDoi(){
     let ab = parseFloat(parseFloat(abEle.value).toFixed(2));
 
 
-    if (Number.isNaN(n1) && dk(n2) && d2>=0 && d1>=0){
+    if (Number.isNaN(n1) && dk(n2) && d2>=0 && d1>=0 && ab>0){
         let kqn1 = Math.round(((n2*d2*(ab+d2))/(d1*(ab+d1)))*100)/100;
         document.getElementById("n1").value = kqn1.toString();
     } 
-    else if(Number.isNaN(n2) && dk(n1) && d2>=0 && d1>=0){
+    else if(Number.isNaN(n2) && dk(n1) && d2>=0 && d1>=0 && ab>0){
         let kqn2 = Math.round(((n1*d1*(ab+d1))/(d2*(ab+d2)))*100)/100;
         document.getElementById("n2").value = kqn2.toString();
     }
-    else if(dk(n1) && dk(n2) && Number.isNaN(d1) && d2>=0){
+    else if(dk(n1) && dk(n2) && Number.isNaN(d1) && d2>=0 && ab>0){
         var kqd1 = Math.round((parseFloat(Math.sqrt(( ab * ((n2 * d2)/n1)*(1 + d2 / ab)+Math.pow((0.5*ab),2)))-0.5*ab))*100)/100;
         document.getElementById("d1").value = kqd1.toString();
         console.log(kqd1);
     }
-    else if(dk(n1) && dk(n2) && Number.isNaN(d2) && d1>=0){
+    else if(dk(n1) && dk(n2) && Number.isNaN(d2) && d1>=0 && ab>0){
         let kqd2 = Math.round((parseFloat(Math.sqrt((ab*((n1*d1)/n2)*(1+d1/ab)+Math.pow((0.5*ab),2)))-0.5*ab))*100)/100;
         document.getElementById("d2").value = kqd2.toString();
         console.log(kqd2)
